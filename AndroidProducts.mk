1fd4626a7fcae0c7471501fcad717338ed40548b
Fevax <fer.vonarx@gmail.com>
2017-01-02 04:28:49 -0300

    hero: Slim-ify

---
+++ AndroidProducts.mk
@@ -0,0 +1,3 @@
+PRODUCT_MAKEFILES := \
+	$(LOCAL_DIR)/hero2lte.mk \
+	$(LOCAL_DIR)/device_hero2lte.mk \
---
+++ slim.mk
@@ -0,0 +1,34 @@
+#
+# Copyright (C) 2017 Fernando Von Arx <fer.vonarx@gmail.com>
+# Copyright (C) 2017 Jesse Chan <cjx123@outlook.com>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/hero2lte/device_hero2lte.mk)
+$(call inherit-product, device/samsung/universal8890/device_universal8890.mk)
+
+# Inherit from the 64 bit configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_NAME := slim_hero2lte
+PRODUCT_DEVICE := hero2lte
+PRODUCT_BRAND := samsung
+PRODUCT_MANUFACTURER := samsung
+PRODUCT_RELEASE_NAME := hero2lte
